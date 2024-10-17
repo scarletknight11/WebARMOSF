da5305abb7ed7f101f9b5a2f21fb8e71ee819106
Hagar Platner <99394976+hagarplatner@users.noreply.github.com>
2024-10-17 23:26:45 +0300

    desert part 1

---
+++ Library/Bee/artifacts/WebGL/il2cppOutput/cpp/Il2CppReversePInvokeWrapperTable.cpp
@@ -0,0 +1,77 @@
+﻿#include "pch-cpp.hpp"
+
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+
+
+
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915;
+
+
+
+IL2CPP_EXTERN_C_BEGIN
+IL2CPP_EXTERN_C_END
+
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#pragma clang diagnostic ignored "-Wunused-variable"
+#endif
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F  : public RuntimeObject
+{
+};
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_pinvoke
+{
+};
+struct ValueType_t6D9B272BD21782F0A9A14F2E41F85A50E97A986F_marshaled_com
+{
+};
+struct IntPtr_t 
+{
+	void* ___m_value;
+};
+struct Void_t4861ACF8F4594C3437BB48B6E56783494B843915 
+{
+	union
+	{
+		struct
+		{
+		};
+		uint8_t Void_t4861ACF8F4594C3437BB48B6E56783494B843915__padding[1];
+	};
+};
+struct IntPtr_t_StaticFields
+{
+	intptr_t ___Zero;
+};
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
+
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_CultureInfo_OnCultureInfoChangedInAppX_m407BCFC1029A4485B7B063BC2F3601968C3BE577(Il2CppChar* ___0_language);
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_MindARFacePlugin_OnARReady_mD78900195684E470BBD8A43D2B39DCD9979F1770();
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_MindARFacePlugin_OnARUpdate_m585555BA6807A6B136BA3DD69427D2AD85FD4A08(int32_t ___0_isFound);
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_MindARFacePlugin_OnCameraConfigChange_mD5C9CC09EA58176B701C5FC53388FD3436D9D52E();
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_MindARImagePlugin_OnARReady_m1C9C95FAEDB3BED37AFF4B3A212A7A35D8E620C3();
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_MindARImagePlugin_OnARUpdate_m3F77BF2A2B10EBE30AA22EB63D477F3250FAC5C7(int32_t ___0_targetIndex, int32_t ___1_isFound);
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_MindARImagePlugin_OnCameraConfigChange_mAD21B5440BADF45BF232E6CE1871EB2392530686();
+extern "C" void DEFAULT_CALL ReversePInvokeWrapper_OSSpecificSynchronizationContext_InvocationEntry_mF93C3CF6DBEC86E377576D840CAF517CB6E4D7E3(intptr_t ___0_arg);
+
+
+IL2CPP_EXTERN_C const Il2CppMethodPointer g_ReversePInvokeWrapperPointers[];
+const Il2CppMethodPointer g_ReversePInvokeWrapperPointers[8] = 
+{
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_CultureInfo_OnCultureInfoChangedInAppX_m407BCFC1029A4485B7B063BC2F3601968C3BE577),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_MindARFacePlugin_OnARReady_mD78900195684E470BBD8A43D2B39DCD9979F1770),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_MindARFacePlugin_OnARUpdate_m585555BA6807A6B136BA3DD69427D2AD85FD4A08),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_MindARFacePlugin_OnCameraConfigChange_mD5C9CC09EA58176B701C5FC53388FD3436D9D52E),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_MindARImagePlugin_OnARReady_m1C9C95FAEDB3BED37AFF4B3A212A7A35D8E620C3),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_MindARImagePlugin_OnARUpdate_m3F77BF2A2B10EBE30AA22EB63D477F3250FAC5C7),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_MindARImagePlugin_OnCameraConfigChange_mAD21B5440BADF45BF232E6CE1871EB2392530686),
+	reinterpret_cast<Il2CppMethodPointer>(ReversePInvokeWrapper_OSSpecificSynchronizationContext_InvocationEntry_mF93C3CF6DBEC86E377576D840CAF517CB6E4D7E3),
+};
